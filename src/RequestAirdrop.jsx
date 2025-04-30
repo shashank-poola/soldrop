fb9486cd0d1d237c214276cabb05e3f989575088
shashank-poola <shashankpoola123@gmail.com>
2025-04-30 18:00:41 +0530

    Inital commits to soldrop

---
+++ src/RequestAirdrop.jsx
@@ -0,0 +1,20 @@
+import { useWallet } from "@solana/wallet-adapter-react";
+import { useConnection } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+export function RequestAirdrop() {
+    const wallet = useWallet();
+    const { connection } = useConnection();
+
+    async function requestAirdrop() {
+        let amount = document.getElementById("amount").value;
+        await connection.requestAirdrop(wallet.publicKey, amount * LAMPORTS_PER_SOL);
+        alert("Airdropped " + amount + " SOL to " + wallet.publicKey.toBase58());
+    }
+
+    return <div>
+        <br/><br/>
+        <input id="amount" type="text" placeholder="Amount" />
+        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 onClick={requestAirdrop}">Request Airdrop</button>
+    </div>
+}
